545aee626bf79e71888a985b9682a3de9e47ee6d
ktcotz <naskret.kamil@gmail.com>
2025-07-15 22:19:49 +0200

    feat: winston, validation pipe
    Add custom logger via winston, add validation pipe with
    class transformers.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from './config/env';
 import { setupSwagger } from './swagger/swagger.config';
+import { logger } from './logger/logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger });
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   setupSwagger(app);
 
   await app.listen(env.PORT);
+
+  Logger.log(`Server running on http://localhost:${env.PORT}`);
 }
 
 bootstrap();

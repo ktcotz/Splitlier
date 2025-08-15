e80688157c10bb5728a3a0f6d1a56c293a009f4b
ktcotz <naskret.kamil@gmail.com>
2025-08-15 10:47:21 +0200

    feat: throttler, helmet, cors
    Add throttling with setup, helmet with prod/dev restriction.
    Add cors based on prod/dev.

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,52 @@ import { env } from './config/env';
 import { setupSwagger } from './swagger/swagger.config';
 import { LoggerService } from './logger/logger.service';
 import { AllExceptionsFilter } from './common/all-exception-filter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const logger = new LoggerService();
 
   const app = await NestFactory.create(AppModule, { logger });
+  const isProd = env.NODE_ENV === 'production';
+
+  if (isProd) {
+    app.use(
+      helmet({
+        contentSecurityPolicy: false,
+        crossOriginOpenerPolicy: { policy: 'same-origin' },
+        crossOriginResourcePolicy: { policy: 'same-site' },
+        crossOriginEmbedderPolicy: false,
+        hsts: { maxAge: 31536000, includeSubDomains: true, preload: true },
+        referrerPolicy: { policy: 'no-referrer' },
+        noSniff: true,
+        xssFilter: true,
+      }),
+    );
+  } else {
+    app.use(
+      helmet({
+        contentSecurityPolicy: false,
+        crossOriginOpenerPolicy: false,
+        crossOriginResourcePolicy: { policy: 'cross-origin' },
+        crossOriginEmbedderPolicy: false,
+        referrerPolicy: { policy: 'no-referrer' },
+      }),
+    );
+  }
+
+  app.enableCors(
+    isProd
+      ? {
+          origin: ['https://api.splitlier.com'],
+          methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+          allowedHeaders: ['Content-Type', 'Authorization'],
+        }
+      : {
+          origin: '*',
+          methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+          allowedHeaders: ['Content-Type', 'Authorization'],
+        },
+  );
 
   app.useGlobalFilters(new AllExceptionsFilter(logger));
   app.useGlobalPipes(
